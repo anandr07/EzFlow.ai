1b5432cea3ef4e22c82120fcf9543a5e4f9c917f
anandr07 <anandraj.1rn17ee005@gmail.com>
2024-02-18 20:52:52 -0500

    Updated data_visualization_controller

--- app/controllers/data_visualization_controller.py
+++ app/controllers/data_visualization_controller.py
@@ -13,17 +13,3 @@ def data_visualization():
     return render_template('data_visualization.html')
 
 
-# Import necessary libraries
-# import pandas as pd
-# from flask import render_template
-# from app import app
-
-# # Sample continuous data for demonstration
-# continuous_data = {'Age': [25, 30, 35, 40, 45],
-#                    'Income': [50000, 60000, 70000, 80000, 90000]}
-
-# @app.route('/data-visualization')
-# def data_visualization():
-#     # Pass continuous variable names and data to the template
-#     continuous_variable_names = list(continuous_data.keys())
-#     return render_template('data_visualization.html', continuous_variable_names=continuous_variable_names)
